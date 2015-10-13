a775ad2fccb8cfd854f41e8f90dc1ce7d05283e7
Neil <nokhand@gmail.com>
2015-10-13 01:03:31 -0500

    Added tree_from_file.py
    Basically runs a parser for a psuedo-language composed only of if and else conditionals
    Intended as an easy-to-write decision tree maker for the engies
    Reads the tree from input.txt, reads the current values from vals.txt
    
    In no way optimized, runs at O(mn), where n is the number of nodes and m is the number of decisions that have to be made. It's not actually stored as a tree, just a flat array.
    
    works tho

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 
+state = {}
 # this is the function determining movement
 # for testing, all are just going to the highest question number
 # in practice, when instantiating our nodes, we'd add how it determines
@@ -79,6 +80,121 @@ def find_level(variables):
     return root.find_level(variables)
 
 
+def read():
+    class item:
+        def __init__(self, cat, desc, funcs):
+            self.cat = cat
+            self.desc = desc
+            self.funcs = funcs
+        def getNext(self, state):
+            if self.funcs:
+                for func in self.funcs:
+                    nextName = func.act(state)
+                    if nextName:
+                        return nextName
+                raise Exception("No valid move from " + self.cat + " but not a leaf")
+            else:
+                return None
+
+    class func:
+        def __init__(self, name, act, val, out):
+            self.name = name
+            self.val = val
+            self.out = out
+            if act == "eq":
+                self.act = self.eq
+            elif act == "gt":
+                self.act = self.gt
+            elif act == "gteq":
+                self.act = self.gteq
+            elif act == "lt":
+                self.act = self.lt
+            elif act == "lteq":
+                self.act = self.lteq
+            elif act == "else":
+                self.act = self.rest
+        def eq(self,state):
+            return self.out if state[self.name] == self.val else None
+        def gt(self,state):
+            return self.out if state[self.name] > self.val else None
+        def gteq(self,state):
+            return self.out if state[self.name] >= self.val else None
+        def lt(self,state):
+            return self.out if state[self.name] < self.val else None
+        def lteq(self,state):
+            return self.out if state[self.name] <= self.val else None
+        def rest(self,state):  # else .. the rest
+            return self.out
+
+    def readValues(f):
+        states = {}
+        for line in f:
+            line = line.strip()
+            words = line.split(":")
+            words = [word.strip() for word in words]
+            states[words[0]] = word[1]
+        return states
+
+    def readFunc(line):
+        words = line.split(" ")
+        words = [word.strip() for word in words]
+        act = { "=":"eq",
+                ">": "gt",
+                ">=": "gteq",
+                "<": "lt",
+                "<=": "lteq"}
+        action = ""
+        if words[0] == "if":
+            if words[2]:
+                name = words[1]
+                action = act[words[2]]
+                val = words[3]
+                target = words[5]
+        elif words[0] == "else":
+            if words[1]:
+                name = None
+                action = "else"
+                val = None
+                target = words[1]
+        else:
+            raise Exception(line + " can't be parsed as a function")
+        func2 = func(name, action, val, target)
+        return func2
+
+    def readItems(f):
+        items = []
+        for line in f:
+            line.strip()
+            if line:
+                cat = line
+                desc = next(f).strip()
+                funcs = list()
+                while True:
+                    line = next(f).strip()
+                    if line:
+                        func = readFunc(line)
+                        funcs.append(func)
+                    else:
+                        break
+                newitem = item(cat, desc, funcs)
+                items.append(newitem)
+        return items
+
+    global state
+    with file("vals.txt", "r") as f:
+        state = readValues(f)
+    with file("input.txt", "r") as f:
+        items = readItems(f)
+
+    print state
+    for item in items:
+        a = item.getNext(state)
+        if a:
+            print a
+        else:
+            print item.cat, "LEAF"
+        #print item.cat, item.desc
+
 root = Node("root")
 A = Node("Question01")
 B = Node("Question02")
@@ -99,8 +215,10 @@ D.add_children([E, C, F])
 F.add_children([L, G, I])
 C.add_children([L, K, J])
 
+read()
+
 # because lazy; if any args, print for graphing
-if sys.argv[1:]:
-    print graphviz_out()
-else:
-    print find_level(None).description
+#  if sys.argv[1:]:
+#      print graphviz_out()
+#  else:
+#      print find_level(None).description
